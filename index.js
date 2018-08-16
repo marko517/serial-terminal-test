bda4151df950cade33bf31f11fa0cd53351ea367
Unknown <miah.maissa@ln0ut.com>
2018-08-16 13:46:03 +0300

    Added functionality for serial connections
    Back-end is complete
    Missing Sending and Receiving on Front-end

--- index.js
+++ index.js
@@ -3,45 +3,78 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var SerialPort = require('serialport');
+var Readline = require('@serialport/parser-readline')
 
 app.use(express.static('public'));
 app.use('/', express.static('public'));
 app.use('/lib', express.static('public/bower_components'));
 
-http.listen(3000, function(){
-    console.log('App listening on port 3000');
-  });
+var SerialConnection = null;
+var connectedSocket = null;
 
-io.on('connection', function(socket){
+http.listen(3000, function () {
+    console.log('App listening on port 3000');
+});
+
+io.on('connection', function (socket) {
     console.log("New Connection");
     sendPorts(socket);
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('Connection closed');
     });
 
-    socket.on('getPorts', function(){
+    socket.on('getPorts', function () {
         console.log("Sending Ports");
         sendPorts(socket);
     });
 
-    socket.on('text', function(str){
+    socket.on('text', function (str) {
         console.log("Received " + str);
         socket.emit('text', str);
+    });
+
+    socket.on('comConnect', comConnect(comObj));
+
+    socket.on('comDisconnect', function () {
+        console.log("COM Disconnect");
+        SerialConnection.close();
+        SerialConnection = null;
+    })
+
+    socket.on('comSend', function (str) {
+        console.log("Sending " + str);
+        SerialConnection.write(str);
     })
 });
 
-function sendPorts(socket){
-    SerialPort.list(function (errors, ports){
-        console.log("Begin Ports\n");
-        if(socket == null)
-        {
+function comConnect(comObj) {
+    console.log("COM Connect " + comObj.comName);
+    if (SerialConnection == null) {
+        SerialConnection = new SerialPort(comObj.comName, comObj);
+        parser = SerialConnection.pipe(new Readline({ delimiter: '\r\n' }))
+        parser.on('data', SerialDataCallback);
+        connectedSocket = socket;
+        socket.emit('comConnectAccept');
+        console.log("Serial Connection Established On: " + comObj.comName);
+    }
+    else {
+        console.log("Serial Connection Already Established On Other Port");
+        socket.emit('comConnectRefuse');
+    }
+}
+
+function sendPorts(socket) {
+    SerialPort.list(function (errors, ports) {
+        if (socket == null) {
             io.emit('ports', ports);
         }
-        else
-        {
+        else {
             socket.emit('ports', ports);
         }
-        console.log(ports);
     });
+}
+
+function SerialDataCallback(str) {
+    connectedSocket.emit('comReceive', str);
 }
--- public/js/app.js
+++ public/js/app.js
@@ -9,11 +9,25 @@ app.service('SocketService', ['socketFactory', function SocketService(socketFact
 app.controller('homeController', function ($scope, localStorageService, SocketService) {
 
     $scope.array = [];
-    $scope.selectedPort;
+    $scope.selectedPort = null;
     $scope.messages = ["asd", "dddd"];
+    $scope.description;
+    $scope.connectButtonText = "Connect";
+    $scope.dropDownDisable = false;
 
-    $scope.connect = function () {
-        // TODO: Add Connect Functionality
+    $scope.bitrate = "9600";
+    $scope.dataBit = "8";
+    $scope.Parity = "even";
+    $scope.StopBit = "1";
+    errElement = document.getElementById('errParagraph');
+
+    $scope.connectButton = function () {
+        if ($scope.connectButtonText == "Connect") {
+            connect();
+        }
+        else {
+            disconnect();
+        }
     }
 
     $scope.getPorts = function () {
@@ -26,6 +40,65 @@ app.controller('homeController', function ($scope, localStorageService, SocketSe
 
     SocketService.on('ports', function (portList) {
         $scope.array = portList;
-    })
+    });
 
+    SocketService.on('comConnectAccept', function () {
+        errElement.innerHTML = "Connection established";
+        $scope.connectButtonText = "Disconnect";
+        $scope.dropDownDisable = true;
+    });
+
+    SocketService.on('comConnectRefuse', function () {
+        errElement.innerHTML = "Connection refused";
+    });
+
+    SocketService.on('comDisconnect', function () {
+        errElement.innerHTML = "Connection closed from other side";
+        $scope.connectButtonText = "Connect";
+        $scope.dropDownDisable = false;
+    });
+
+    function connect() {
+        if ($scope.selectedPort != null) {
+            if ($scope.bitrate != null) {
+                if ($scope.dataBit != null) {
+                    if ($scope.Parity != null) {
+                        if ($scope.StopBit != null) {
+                            var comConnection = {
+                                "comName": $scope.selectedPort,
+                                "baudRate": parseInt($scope.bitrate, 10),
+                                "dataBits": parseInt($scope.dataBit, 10),
+                                "parity": $scope.Parity,
+                                "stopBits": parseInt($scope.StopBit, 10)
+                            };
+                            errElement.innerHTML = "Connecting...";
+                            SocketService.emit('comConnect', comConnection);
+                        }
+                        else {
+                            errElement.innerHTML = "You have not chosen stop bits";
+                        }
+                    }
+                    else {
+                        errElement.innerHTML = "You have not chosen parity";
+                    }
+                }
+                else {
+                    errElement.innerHTML = "You have not chosen data bits";
+                }
+            }
+            else {
+                errElement.innerHTML = "You have not chosen bitrate";
+            }
+        }
+        else {
+            errElement.innerHTML = "You have not chosen a port";
+        }
+    }
+
+    function disconnect() {
+        SocketService.emit('comDisconnect');
+        errElement.innerHTML = "Connection closed";
+        $scope.connectButtonText = "Connect";
+        $scope.dropDownDisable = false;
+    }
 })
